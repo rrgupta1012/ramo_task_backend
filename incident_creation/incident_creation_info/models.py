4ae6c558937be1f7b7eaa878476aaa9555fbf4d5
Ravi Gupta <rrgupta1012@gmail.com>
2020-02-12 18:19:16 +0530

    name updated

--- incident_creation/incident_creation_info/models.py
+++ incident_creation/incident_creation_info/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class IncidentCreationInfo(models.Model):
-    first_name = models.CharField('First Nameb', max_length=10, null=True, blank=True)
+    first_name = models.CharField('First Name', max_length=10, null=True, blank=True)
 
     def __str__(self):
         return self.first_name
